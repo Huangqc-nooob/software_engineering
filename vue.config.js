07df80cef6640694e5145db1f171c71c4f62b4dd
Huangqc-nooob <1553636643@qq.com>
2024-11-02 23:59:20 +0800

    feat: feat: feat: feat(整体范围): 整体文件跟视频做到P10

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// webpack.config.js
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
+module.exports = {
+  // npm install unplugin-auto-import@0.5.11 --save-dev  
+  //npm install unplugin-vue-components@0.17.11 --save-dev
+  configureWebpack: config => {
+    config.plugins.push(AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }))
+    config.plugins.push(Components({
+        resolvers: [ElementPlusResolver()],
+      }))
+  }
+}
